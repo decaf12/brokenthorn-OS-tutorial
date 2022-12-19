8355bcac4b563caf70ef89e9f54ad42730c953ab
decaf <siyang.he.uw@gmail.com>
2022-12-18 23:38:54 -0500

    blundell: conditionally print character

--- Boot1_blundell.asm
+++ Boot1_blundell.asm
@@ -5,46 +5,30 @@
 ;	Operating Systems Development Tutorial
 ;*********************************************
 mov ah, 0x0e
-mov bp, 0x8000
-mov sp, bp
+mov bx, 40
 
-push 'A'
-push 'B'
-push 'C'
+cmp bx, 4
+jle print_A
+jg check_B
 
-; Pop the stack and save the popped character in al
-; We would *like* to just do "pop al", but we can't.
-; 
-; Remember we are in 16-bit mode so we can only pop
-; 16 bits at a time, and al is an 8-bit register.
-; 
-; So we must pop into a 16-bit register such as ax, bx, etc.
-; But we can't directly pop into ax either, as that risks overwriting
-; ah, which should keep holding 0x0e.
-; 
-; So pop into another 16-bit register (randomly choose bx here),
-; then move its lower byte to al.
-
-pop bx
-mov al, bl
-int 0x10
-
-pop bx
-mov al, bl
-int 0x10
-
-; For the sake of exercise, try directly referencing the address of the
-; last character.
-
-; Where is the last character?
-; Recall we set the stack base to be 0x8000
-; The 'A' is represented as two bytes in 16-bit mode:
-;   1) zero padding at 0x7fff
-;   2) The actual character 'A' at 0x7ffe
-; We want to put the actual charater into al.
-mov al, [0x7ffe]
+print:
 int 0x10
 jmp $
+
+print_A:
+    mov al, 'A'
+    jmp print
+
+check_B:
+    cmp bx, 40
+    jl print_B
+    jge print_C
+print_B:
+    mov al, 'B'
+    jmp print
+print_C:
+    mov al, 'C'
+    jmp print
 ; $: address of the current line
 ; $$: address of the first instruction. We set this at 0x7c00.
 ; $ - $$: the number of bytes from the current line to the start (aka size of the program)
