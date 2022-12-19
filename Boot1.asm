a2291fb7b41ea3e31d8fe0837016f6918f94517b
decaf <siyang.he.uw@gmail.com>
2022-12-18 21:23:00 -0500

    boot1.asm print msg to screen

--- Boot1.asm
+++ Boot1.asm
@@ -1,56 +1,42 @@
-;*********************************************
-;	Boot1.asm
-;		- A Simple Bootloader
-;
-;	Operating Systems Development Tutorial
-;*********************************************
-
-; org = origin. Setting org to 0x7c00 puts this file at address 0x7c00,
-; which, given this is a bootloader, is exactly where it should be.
 org 0x7c00
-
-; the "bits 16" directive tells NASM to generate code under 16-bit mode
-; Why do this? For backward compatibility, all x86-compatible computers
-; boot into 16-bit mode. Later on we'll switch to 32-bit mode.
 bits 16
+start: jmp loader
+msg	db	"decafOS", 0
 
-; cli: clear all interrupts
-; hlt: halt the system
-Start:
-; 0x10 is the interrupt for printing a to the screen
-; parameters:
-;   mode: stored in ah. Here we use 0x0e, which means tele-type mode
-;   ASCII code: stored in al.
-; 
-; To print a whole string of characters, we leave ah at 0x0e, and repeatedly
-; change al and call int 0x10.
-    mov ah, 0x0e
-    mov al, 'd'
-    int 0x10
-    mov al, 'e'
-    int 0x10
-    mov al, 'c'
-    int 0x10
-    mov al, 'a'
-    int 0x10
-    mov al, 'f'
-    int 0x10
-    mov al, 'O'
-    int 0x10
-    mov al, 'S'
-    int 0x10
-    cli
-    hlt
+;***************************************
+;	Prints a string
+;	DS=>SI: 0 terminated string
+;***************************************
 
-; $: address of the current line
-; $$: address of the first instruction. We set this at 0x7c00.
-; $ - $$: the number of bytes from the current line to the start (aka size of the program)
-; 
-; The goal here is to zero-fill all unused bytes of the sector (512 bytes).
-; On the boot sector we have:
-; 1) The program itself
-; 2) A bunch of padding zeros up to the 510th byte, leaving the last two bytes for:
-; 3) The magic number that identifies the sector as a boot sector.
+Print:
+			lodsb
+			or			al, al				; al=current character
+			jz			PrintDone			; null terminator found
+			mov			ah,	0eh			; get next character
+			int			10h
+			jmp			Print
+PrintDone:
+			ret
 
-times 510 - ($ - $$) db 0
-dw 0xAA55
+;*************************************************;
+;	Bootloader Entry Point
+;*************************************************;
+
+loader:
+
+; Error Fix 1 ------------------------------------------
+
+	xor	ax, ax		; Setup segments to insure they are 0. Remember that
+	mov	ds, ax		; we have ORG 0x7c00. This means all addresses are based
+	mov	es, ax		; from 0x7c00:0. Because the data segments are within the same
+				; code segment, null em.
+
+	mov	si, msg
+	call	Print
+
+	cli			; Clear all Interrupts
+	hlt			; halt the system
+	
+times 510 - ($-$$) db 0		; We have to be 512 bytes. Clear the rest of the bytes with 0
+
+dw 0xAA55			; Boot Signiture
